222463b6a3a08c750b15cf105d114ab291d4b0b8
Dmitry Redkovolosov <dmitry.redkovolosov@dsr-corporation.com>
2021-07-07 22:17:17 +0300

    Add initial logic for processing rounds
    Add 'JOB' action processing logic.
    Add lint:fix script.

--- src/engine.ts
+++ src/engine.ts
@@ -1,7 +1,8 @@
 import { initCards, shuffle } from './utils/deck-utils'
 import { Action, InventionCard, Player, Turn, User } from './types'
+import EventEmitter from 'events'
 
-export class Engine {
+export class Engine extends EventEmitter {
   players: Player[]
   activeDeck: InventionCard[]
   usedDeck: InventionCard[]
@@ -9,7 +10,7 @@ export class Engine {
 
   // TODO: Define necessary options
   constructor (users: User[], options: any) {
-    // TODO: Use internal structure of players with all the necessary data populated, require from input only functions
+    super()
     this.players = []
     for (const user of users) {
       this.players.push({
@@ -34,7 +35,8 @@ export class Engine {
 
   async start (): Promise<User> {
     this.activeDeck = shuffle(this.activeDeck)
-    this._distributeCards()
+    await this._distributeCards()
+    await Promise.all(this.players.map(async (player) => await player.user.setMoney(player.money)))
     let winner: User | null = null
     do {
       await this._playRound()
@@ -43,37 +45,62 @@ export class Engine {
     return winner
   }
 
-  private _distributeCards (): void {
-    // TODO: give first cards to players
+  private async _distributeCards (): Promise<void> {
+    // Give 4 initial invention cards to every player
+    for (let i = 0; i < 4; i++) {
+      // Go through every user and give one card at a time
+      for (const player of this.players) {
+        const card = this.activeDeck.pop()
+        if (card !== undefined) {
+          player.cards.push(card)
+        }
+      }
+    }
+    // Distribute these cards to users
+    await Promise.all(this.players.map(async (player) => await player.user.giveCards(player.cards)))
   }
 
   private async _playRound (): Promise<void> {
-    await Promise.all(this.players.map(async (player) => await player.user.turn()))
-    // TODO: Go through player turns, process them phase by phase
+    const userTurns: Turn[] = await Promise.all(this.players.map(async (player) => await player.user.turn()))
+    this.emit('turn', userTurns)
+    await this._processSpies(userTurns)
+    await this._espionagePhase(userTurns)
+    await this._inventionPhase(userTurns)
+    await this._researchPhase(userTurns)
+    await this._jobPhase(userTurns)
   }
 
   /**
    * Process current spies
    * @private
    */
-  private _processSpies (turns: Turn[]): void {
-    // TODO: Implement me + implement spies storage
+  private async _processSpies (turns: Turn[]): Promise<void> {
+    await Promise.all(this.players.map((player, index) => {
+
+    }))
   }
 
-  private _espionagePhase (): void {
+  private async _espionagePhase (userTurns: Turn[]): Promise<void> {
     // TODO: Process turn for players who have chosen espionage
   }
 
-  private _inventionPhase (): void {
+  private async _inventionPhase (userTurns: Turn[]): Promise<void> {
     // TODO: Process turn for players who have chosen invention, process effects
   }
 
-  private _researchPhase (): void {
+  private async _researchPhase (userTurns: Turn[]): Promise<void> {
     // TODO: Process turn for players who have chosen research
   }
 
-  private _jobPhase (): void {
-    // TODO: Process turn for players who have chosen job
+  private async _jobPhase (userTurns: Turn[]): Promise<void> {
+    await Promise.all(userTurns.map(async (turn, index) => {
+      if (userTurns[index].action !== Action.JOB) {
+        return
+      }
+      this.players[index].money += 4
+      await this.players[index].user.setMoney(this.players[index].money)
+      this.emit('job', index)
+    }))
   }
 
   private _findWinner (): User | null {
