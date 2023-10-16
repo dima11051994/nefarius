533f41f4a7e404b1becc0abc5c44c00e195862d1
Dmitry Redkovolosov <dmitry.redkovolosov@dsr-corporation.com>
2023-10-17 01:35:02 +0300

    Export almost everything from src

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
-import { Engine } from './engine'
-
-export { Engine }
+export * from './src/engine'
+export * from './src/player'
+export * from './src/types'
+export * from './src/example/clients/console'
